13acfe174d96126c85fa8a17434db2f5ef92e6f9
Eric Haskins <eric@ehaskins.com>
2016-01-13 01:05:28 -0600

    Solve problem 13

---
+++ Problem013.fsx
@@ -0,0 +1,7 @@
+#load "Problem013.Resources.fs"
+module Problem13 =
+    let answer = 
+        Problem013.Resources.numberStrings.Split '\n' 
+        |> Seq.map (fun num -> bigint.Parse num) 
+        |> Seq.fold (fun a b -> a + b) bigint.Zero
+        |> fun bi -> bi.ToString().Substring(10)
